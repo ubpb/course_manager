0b0160590b141efe9a42d584518bee72997a6b85
René Sprotte <renspr@users.noreply.github.com>
2025-04-24 15:00:17 +0200

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
-  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "title", null: false
     t.string "color_code", null: false
     t.integer "position"
@@ -21,7 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["title"], name: "index_categories_on_title", unique: true
   end
 
-  create_table "certificates", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "certificates", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "registration_id"
     t.string "digest", null: false
     t.string "initials", null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["registration_id"], name: "index_certificates_on_registration_id"
   end
 
-  create_table "certifications", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "certifications", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "event_id", null: false
     t.text "learning_results"
     t.string "signature"
@@ -39,7 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["event_id"], name: "index_certifications_on_event_id", unique: true
   end
 
-  create_table "consultings", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "consultings", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "category_id"
     t.string "title", null: false
     t.boolean "published", default: false, null: false
@@ -53,21 +53,21 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["published"], name: "index_consultings_on_published"
   end
 
-  create_table "consultings_target_groups", id: false, charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "consultings_target_groups", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "consulting_id", null: false
     t.bigint "target_group_id", null: false
     t.index ["consulting_id"], name: "fk_rails_1066feeb42"
     t.index ["target_group_id"], name: "fk_rails_1ff2a90d06"
   end
 
-  create_table "consultings_topics", id: false, charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "consultings_topics", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "consulting_id", null: false
     t.bigint "topic_id", null: false
     t.index ["consulting_id"], name: "fk_rails_c462aeade9"
     t.index ["topic_id"], name: "fk_rails_e568752df1"
   end
 
-  create_table "courses", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "courses", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "category_id"
     t.string "title", null: false
     t.boolean "published", default: false, null: false
@@ -81,21 +81,21 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["published"], name: "index_courses_on_published"
   end
 
-  create_table "courses_target_groups", id: false, charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "courses_target_groups", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "course_id", null: false
     t.bigint "target_group_id", null: false
     t.index ["course_id"], name: "fk_rails_26bff39dfc"
     t.index ["target_group_id"], name: "fk_rails_2fafdf950e"
   end
 
-  create_table "courses_topics", id: false, charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "courses_topics", id: false, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "course_id", null: false
     t.bigint "topic_id", null: false
     t.index ["course_id"], name: "fk_rails_0f23d0d39a"
     t.index ["topic_id"], name: "fk_rails_da38b9ed46"
   end
 
-  create_table "events", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "events", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "course_id", null: false
     t.integer "registrations_count", default: 0, null: false
     t.datetime "date_and_time", null: false
@@ -113,7 +113,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["published"], name: "index_events_on_published"
   end
 
-  create_table "registrations", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "registrations", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "event_id", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -129,7 +129,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["event_id"], name: "index_registrations_on_event_id"
   end
 
-  create_table "reports", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "reports", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "event_id", null: false
     t.integer "duration", null: false
     t.integer "number_of_participants", null: false
@@ -149,7 +149,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["event_id"], name: "index_reports_on_event_id", unique: true
   end
 
-  create_table "target_groups", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "target_groups", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "title", null: false
     t.integer "position"
     t.datetime "created_at", null: false
@@ -158,7 +158,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_07_133514) do
     t.index ["title"], name: "index_target_groups_on_title", unique: true
   end
 
-  create_table "topics", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
+  create_table "topics", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "title", null: false
     t.integer "position"
     t.datetime "created_at", null: false
