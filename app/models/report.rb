2aabbaef860ca6ca13ca00e84ba22a8fe04d90c6
René Sprotte <renspr@users.noreply.github.com>
2026-06-25 15:47:30 +0200

    Better presence validation.
    Closes ubpb/course_manager#25

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,10 +6,17 @@ class Report < ApplicationRecord
   # Validations
   validates :duration, presence: true, numericality: {greater_than_or_equal_to: 0}
   validates :number_of_participants, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validates :lecturer, presence: true
-  validates :lecturer_md, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validates :lecturer_gd, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validates :lecturer_hd, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :lecturer, presence: false
+  validates :lecturer_md, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :lecturer_gd, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :lecturer_hd, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :presence_types, presence: true
+  validates :organization_types, presence: true
+  validates :levels, presence: true
+  validates :categories, presence: true
+  validates :forms, presence: true
+  validates :audiences, presence: true
+  validates :focus, presence: true
 
   # Flags
   flag :presence_types,      [:presence,
