e2dcda1c09ba72b81122892a4ce8ff132a1be1f0
René Sprotte <renspr@users.noreply.github.com>
2026-07-09 15:10:10 +0200

    Do not be selective on offer form about the type.

--- app/assets/src/application/js/stimulus/application.js
+++ app/assets/src/application/js/stimulus/application.js
@@ -24,10 +24,6 @@ application.register("checkbox-select-all", CheckboxSelectAll)
 import FilterForm from "./filter_form_controller.js"
 application.register("filter-form", FilterForm)
 
-// Register OfferForm controller
-import OfferForm from "./offer_form_controller.js"
-application.register("offer-form", OfferForm)
-
 // Register Tooltip controller
 import TooltipController from "./tooltip_controller.js"
 application.register("tooltip", TooltipController)
